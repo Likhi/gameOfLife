e53fcd5fd9f21011bbf0dff0cbeb4dff0873a464
itmelo <likhi.ondov@gmail.com>
2022-08-24 10:49:29 -0700

    Clean up main.go a bit for clarity and remove unncessary history tracking

--- cmd/main.go
+++ cmd/main.go
@@ -9,21 +9,21 @@ import (
 //todo more oscillator test cases
 //todo update same space on terminal screen (i.e no new line)
 
+const (
+	_period     = 500 * time.Millisecond
+	_iterations = 50
+)
+
 func main() {
 	//g := gol.NewGrid(61, 100)
 	g := gol.NewOscillator(gol.BeaconPeriodTwo)
-	history := []gol.Grid{}
 	fmt.Println("start")
 	g.Print()
 
-	const _iterations = 50
-
 	for i := 0; i < _iterations; i++ {
-		// save last grid state
-		history = append(history, g)
-		fmt.Println("Iteration:", i)
-		g = g.Run()
-		g.Print()
-		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Iteration:", i) // Show which iteration we're on
+		g = g.Run()                  // Run the simulation once
+		g.Print()                    // Show simulation after one iteration
+		time.Sleep(_period)          // Slow your roll
 	}
 }
